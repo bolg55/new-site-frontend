db69db023418cf9855a1dc407e81ae0358210618
Kellen Bolger <bolger.kellen@gmail.com>
2021-07-07 02:40:04 -0400

    added footer elements

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,51 @@
+import styles from "../styles/Footer.module.css"
+import {
+  FaInstagramSquare,
+  FaLinkedin,
+  FaTwitterSquare,
+  FaGithubSquare,
+} from "react-icons/fa"
+
+const data = [
+  {
+    id: 1,
+    icon: <FaInstagramSquare className='social-icon'></FaInstagramSquare>,
+    url: "https://www.instagram.com/kellenbolger",
+  },
+  {
+    id: 2,
+    icon: <FaLinkedin className='social-icon'></FaLinkedin>,
+    url: "https://www.linkedin.com/in/kellenbolger",
+  },
+  {
+    id: 3,
+    icon: <FaTwitterSquare className='social-icon'></FaTwitterSquare>,
+    url: "https://www.twitter.com/kellenbolger",
+  },
+  {
+    id: 4,
+    icon: <FaGithubSquare className='social-icon'></FaGithubSquare>,
+    url: "https://github.com/bolg55",
+  },
+]
+
+const links = data.map((link) => {
+  return (
+    <li key={link.id}>
+      <a href={link.url} className={styles["social-link"]}>
+        {link.icon}
+      </a>
+    </li>
+  )
+})
+
 export default function Footer() {
   return (
-    <div>
+    <div className={styles.footer}>
       <p>Copyright &copy; 2021 Kellen Bolger</p>
+      <div className={styles.social}>
+        <ul>{links}</ul>
+      </div>
     </div>
   )
 }
